3db7269e3d42f03b0b3b9a16576f52bfaae5d155
Harsh Jha <hjha0695@gmail.com>
2023-06-04 00:29:53 +0530

    done with react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-
 import './App.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Courses from './components/Courses';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Signup from "./components/Signup";
+import Landing from "./pages/landing";
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+  ]);
 
 function App() {
   return (
     <div className="font-sans">
-      <Navbar />
-      <Hero />
-      <Courses />
-      <Footer />
+        <Navbar />
+      <RouterProvider router={router} />
+        <Footer />
     </div>
   );
 }
